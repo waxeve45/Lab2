682c7fb80b82a4f63519f55c192f9a8b11693b24
waxeve45 <sriboonruang2545@gmail.com>
2023-01-09 12:20:25 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="show.php">
+        what is your name:<input type="text"name="name"><br>
+        what is your age:<input type="text"name="age"><br>
+        <input type="submit" value="submit">
+
+    </form>
+
+
+</body>
+</html>
+
+    
---
+++ show.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    $name = $_GET["name"];
+    $age = $_GET["age"];
+
+    echo "Name:".$name."<br>";
+    echo "Age".$age."<br>";
+
+    ?>
+
+</body>
+</html>
