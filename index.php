d4ce788434c31d02103e6e26e033c487cd00d026
waxeve45 <120999602+waxeve45@users.noreply.github.com>
2023-01-14 23:42:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,15 @@
     
     
 
-
-
     what is your name:   <input type="text"name="name"><br>
     what is your age:    <input type="text"name="age"><br>
-    what is your sex:    <input type="text"name="sex"><br>
+    what is your sex:    <select name="sex">
+        <option value="">-เลือก-</option>
+        <option value="ชาย">-ชาย-</option>
+        <option value="หญิง">-หญิง-</option>
+            
+    </select>
+    <br>
     what is your status: <input type="text"name="status"><br>
     <input type="submit" value="submit"> 
     
