1cd441e2345010dca5a7c8ed62c0080a54697c0b
waxeve45 <120999602+waxeve45@users.noreply.github.com>
2023-01-14 22:55:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <form action="show.php">
-        what is your name:<input type="text"name="name"><br>
-        what is your age:<input type="text"name="age"><br>
-        <input type="submit" value="submit">
+<form action="show.php"> 
 
-    </form>
+<select name="prefix">
+        <option value="">--เลือก--</option>
+        <option value="ดช.">--ดช.--</option>
+        <option value="ดญ.">--ดญ.--</option>
+        <option value="นาย">--นาย--</option>
+        <option value="นาง">--นาง--</option>
+        <option value="นางสาว">--นางสาว--</option> 
+            
+    </select>
+    <br>
+    
+    
+
+
+
+    what is your name:   <input type="text"name="name"><br>
+    what is your age:    <input type="text"name="age"><br>
+    what is your sex:    <input type="text"name="sex"><br>
+    what is your status: <input type="text"name="status"><br>
+    <input type="submit" value="submit"> 
+    
+</form>
 
 
 </body>
