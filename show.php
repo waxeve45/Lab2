8769801288ab1f0eb3934ae31916fd8a689f3306
waxeve45 <120999602+waxeve45@users.noreply.github.com>
2023-01-14 22:55:38 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <?php
+<?php 
+    $prefix = $_GET["prefix"];
     $name = $_GET["name"];
     $age = $_GET["age"];
+    $sex = $_GET["sex"];
+    $status = $_GET["status"];
+ 
+    
+    echo "Name:".$prefix.$name."<br>";
+    echo "Age:".$age."<br>";
+    echo "sex:".$sex."<br>";
+    echo "status:".$status."<br>";
 
-    echo "Name:".$name."<br>";
-    echo "Age".$age."<br>";
-
-    ?>
+?>
 
 </body>
 </html>
